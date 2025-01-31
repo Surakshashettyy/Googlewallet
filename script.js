d9d9179694857f1fac7aa812ed6d3addf46dcd5e
Suraksha Shetty <150645848+Surakshashettyy@users.noreply.github.com>
2025-01-31 19:42:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Generate QR Code
+    new QRCode(document.getElementById("qrcode"), {
+        text: "Laila Doifoo 1234",
+        width: 100,
+        height: 100
+    });
+
+    // Button click event
+    document.getElementById("addToWallet").addEventListener("click", function() {
+        let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        if (/android/i.test(userAgent)) {
+            // Redirect Android users to Google Wallet App
+            window.location.href = "intent://wallet.google.com/#Intent;package=com.google.android.apps.walletnfcrel;scheme=https;end;";
+        } 
+        else {
+            // For Desktop/Laptop users, redirect to Google Wallet website
+            window.location.href = "https://wallet.google/";
+        }
+    });
+});
