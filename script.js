f624c5bd75c3b03088f0c36b2b29185e516dd753
Suraksha Shetty <surakshashetty1974@gmail.com>
2025-02-02 15:21:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function openGoogleWallet() {
-    window.location.href = "intent://#Intent;scheme=googlewallet;package=com.google.android.apps.walletnfcrel;end;";
+    let walletIntent = "intent://#Intent;scheme=googlewallet;package=com.google.android.apps.walletnfcrel;end;";
     
+    // Create an invisible iframe to attempt opening Google Wallet
+    let iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    iframe.src = walletIntent;
+    document.body.appendChild(iframe);
+
+    // If the app is not installed, redirect to Play Store after 2 seconds
     setTimeout(() => {
         window.location.href = "https://play.google.com/store/apps/details?id=com.google.android.apps.walletnfcrel";
-    }, 2000); // Redirects to Play Store if the app isn't installed
+    }, 2000);
 }
